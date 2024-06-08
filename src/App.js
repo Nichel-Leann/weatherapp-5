8a5c24e71bb50bbfd21406c2536ab233cf36ddc2
Nichel-Leann <160755941+Nichel-Leann@users.noreply.github.com>
2024-06-07 21:33:42 -0500

    added links to footer

--- src/App.js
+++ src/App.js
@@ -3,5 +3,38 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/Nichel-Leann"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Nichelleann Richards
+        </a>
+        , is{" "}
+        <a
+          href="https://github.com/Nichel-Leann/Weatherapp-SheCodes"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        <br />
+        and
+        <a
+          href="https://easyshecodesweatherapp.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          hosted on Netlify.
+        </a>
+      </footer>
+    </div>
+  );
 }
