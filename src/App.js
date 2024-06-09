a0189a7fccbeb60a7a8d95536c2588475ac0d157
Nichel-Leann <160755941+Nichel-Leann@users.noreply.github.com>
2024-06-08 22:09:29 -0500

    added html layout

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/Nichel-Leann"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Nichelleann Richards
-        </a>
-        , is{" "}
-        <a
-          href="https://github.com/Nichel-Leann/Weatherapp-SheCodes"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-        <br />
-        and
-        <a
-          href="https://easyshecodesweatherapp.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          hosted on Netlify.
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Nichel-Leann"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Nichelleann Richards
+          </a>
+          , is{" "}
+          <a
+            href="https://github.com/Nichel-Leann/Weatherapp-SheCodes"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://easyshecodesweatherapp.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
